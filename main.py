f902ca0c455a2ba474d5ef27caa5233fd6aac3d2
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2023-06-23 12:27:28 +0500

    imgName variable

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import shutil
 from effects import create_video_from_image, apply_blinking_effect
 # read image
 
-
-img = cv2.imread('1.jpg')
+imgName = '1.jpeg'
+img = cv2.imread(imgName)
 # get image shape
 
 # make a random number
@@ -15,11 +15,13 @@ random_number = random_number + random.randint(0, 10000)
 temp_output = f'temp/{random_number}.mp4'
 
 # image_path, duration, fps, output_path
-create_video_from_image('1.jpg', 10, 30, temp_output)
+if not os.path.exists('temp'):
+    os.makedirs('temp')
+create_video_from_image(imgName, 3, 10, temp_output)
 
 temp_output2 = f'temp/{random_number + 1}.mp4'
 
-apply_blinking_effect(temp_output, 4, 8, temp_output2)
+apply_blinking_effect(temp_output, 2, 2, temp_output2)
 
 # remove f'{random_number}.mp4' from directory temp and move f'{random_number + 1}.mp4' to directory output
 os.remove(temp_output)
