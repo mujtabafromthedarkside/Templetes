41f0d06d54c80417371de0901c70e2f70831e105
Mujtaba <72826134+mujtabafromthedarkside@users.noreply.github.com>
2023-07-03 10:34:06 +0500

    not removing output file at the end, remove previous output file of same name if exists

--- main2.py
+++ main2.py
@@ -19,7 +19,7 @@ if TESTING:
 else:
     random_number = random.randint(0, 10000)
     random_number = random_number + random.randint(0, 10000)
-temp_outputs = [f'temp/{random_number}.mp4']
+temp_outputs = [os.path.join('temp',f'{random_number}.mp4')]
 
 # image_path, duration, fps, output_path
 if not os.path.exists('temp'):
@@ -27,11 +27,11 @@ if not os.path.exists('temp'):
 create_video_from_image(imgName, 5, 30, temp_outputs[-1])
 
 random_number += 1
-temp_outputs.append(f'temp/{random_number}.mp4')
+temp_outputs.append(os.path.join('temp',f'{random_number}.mp4'))
 tv_filter(temp_outputs[-2], 4,5, temp_outputs[-1])
 
 random_number += 1
-temp_outputs.append(f'temp/{random_number}.mp4')
+temp_outputs.append(os.path.join('temp',f'{random_number}.mp4'))
 animated_box(temp_outputs[-2], 1,5, temp_outputs[-1])
 
 # # remove f'{random_number}.mp4' from directory temp and move f'{random_number + 1}.mp4' to directory output
@@ -41,15 +41,23 @@ for i in range(0,len(temp_outputs)-1):
 # move the last file to directory output
 if not os.path.exists('output'):
     os.mkdir('output')
+
+# remove file if it already exists in output folder
+output_path = os.path.join('output', temp_outputs[-1][5:])
+print(f'saving final output to {output_path}')
+if os.path.exists(output_path):
+    os.remove(output_path)
+    
 shutil.move(temp_outputs[-1], 'output')
 
 # play a video
 cap = cv2.VideoCapture(f'output/{os.path.basename(temp_outputs[-1])}')
 fps = cap.get(cv2.CAP_PROP_FPS)
+print(fps)
 
 while cap.isOpened():
     wait_time = int(1000/fps/2)
-    
+
     ret, frame = cap.read()
     if not ret:
         break
@@ -65,10 +73,10 @@ cap.release()
 cv2.destroyAllWindows()
 
 # remove files from a directory output/*
-for filename in os.listdir('output'):
-    file_path = os.path.join('output', filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-    except Exception as e:
-        print('Failed to delete %s. Reason: %s' % (file_path, e))
+# for filename in os.listdir('output'):
+#     file_path = os.path.join('output', filename)
+#     try:
+#         if os.path.isfile(file_path) or os.path.islink(file_path):
+#             os.unlink(file_path)
+#     except Exception as e:
+#         print('Failed to delete %s. Reason: %s' % (file_path, e))
